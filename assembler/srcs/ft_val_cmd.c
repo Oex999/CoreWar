d9d0606967d44421e35ac3312d943afe5edc70dc
Owen EXALL <oexall@e5r6p1.jnb.42.fr>
2016-08-09 14:17:58 +0200

    Validating now checks parameters as well.

--- assembler/srcs/ft_val_cmd.c
+++ assembler/srcs/ft_val_cmd.c
@@ -6,7 +6,7 @@
 /*   By: oexall <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/09 10:58:10 by oexall            #+#    #+#             */
-/*   Updated: 2016/08/09 12:23:06 by oexall           ###   ########.fr       */
+/*   Updated: 2016/08/09 14:15:41 by oexall           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,18 +22,48 @@ static int	ft_cargs(char **params)
 	return (i);
 }
 
+t_arg_type	ft_get_type(char *str)
+{
+	if (str[0] == 'r')
+		return (T_REG);
+	else if (str[0] == '%')
+		return (T_DIR);
+	else
+		return (T_IND);
+	return (0);
+}
+
+static int	ft_check_params(char **params, int no_args, int index)
+{
+	t_arg_type	type;
+	int			i;
+
+	i = -1;
+	while (++i < no_args)
+	{
+		type = g_op_tab[index].type[i];
+		if ((ft_get_type(params[i]) & type) != ft_get_type(params[i]))
+			return (0);
+	}
+	return (1);
+}
+
 int			ft_val_cmd(char *cmd)
 {
+	int		res;
 	char	**params;
 	int		no_args;
 	int		index;
 
+	res = 1;
 	if ((index = ft_is_cmd(cmd)) == -1)
-		return (ft_err("Invalid not found."));
+		return (ft_err("Invalid command."));
 	no_args = g_op_tab[index].no_args;
 	params = ft_strsplit(cmd, ' ');
 	if (no_args != (ft_cargs(params) - 1))
-		return (ft_err("Invalid amount of arguments"));
+		res = (ft_err("Invalid amount of parameters."));
+	if (!ft_check_params(&params[1], no_args, index))
+		res = (ft_err("Invalid parameter type."));
 	ft_deltab(params);
-	return (1);
+	return (res);
 }
