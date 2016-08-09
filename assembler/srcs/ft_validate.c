7b34e92fe3ce670d3416d43faa64a09b77e83645
= <owen@exall.za.net>
2016-08-09 19:52:15 +0200

    Validates the first two rows that must be 1) .name & then 2) .comment

--- assembler/srcs/ft_validate.c
+++ assembler/srcs/ft_validate.c
@@ -6,7 +6,7 @@
 /*   By: oexall <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/09 09:32:48 by oexall            #+#    #+#             */
-/*   Updated: 2016/08/09 11:31:03 by oexall           ###   ########.fr       */
+/*   Updated: 2016/08/09 19:51:26 by oexall           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int			ft_validate(t_input **input)
 	t_input	*list;
 
 	list = *input;
+	if (list->alias != NAME || list->next->alias != COMMENT)
+		return (ft_err("Requires .name & .comment to be first in line."));
 	while (list)
 	{
 		if (list->alias == NAME && !ft_val_prog(list->line))
