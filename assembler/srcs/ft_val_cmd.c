80bd0fd3d0712f658bc30b0494d13dfb22cb2833
Owen EXALL <oexall@e5r6p1.jnb.42.fr>
2016-08-09 12:28:42 +0200

    Adding more validating steps for the CMDS

--- assembler/srcs/ft_val_cmd.c
+++ assembler/srcs/ft_val_cmd.c
@@ -6,19 +6,34 @@
 /*   By: oexall <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/09 10:58:10 by oexall            #+#    #+#             */
-/*   Updated: 2016/08/09 11:31:21 by oexall           ###   ########.fr       */
+/*   Updated: 2016/08/09 12:23:06 by oexall           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <asm.h>
 
-int		ft_val_cmd(char *cmd)
+static int	ft_cargs(char **params)
 {
-	int	index;
+	int	i;
+
+	i = 0;
+	while (params[i])
+		i++;
+	return (i);
+}
+
+int			ft_val_cmd(char *cmd)
+{
+	char	**params;
+	int		no_args;
+	int		index;
 
 	if ((index = ft_is_cmd(cmd)) == -1)
 		return (ft_err("Invalid not found."));
-	ft_putnbr(index);
-	ft_putendl(" INDEX");
+	no_args = g_op_tab[index].no_args;
+	params = ft_strsplit(cmd, ' ');
+	if (no_args != (ft_cargs(params) - 1))
+		return (ft_err("Invalid amount of arguments"));
+	ft_deltab(params);
 	return (1);
 }
