1b428d751c9ccd29fe985436fb8057c40cb461b5
= <owen@exall.za.net>
2016-08-10 13:29:06 +0200

    Added checks to ensure that variable were allocated

--- assembler/srcs/ft_output_free.c
+++ assembler/srcs/ft_output_free.c
@@ -6,7 +6,7 @@
 /*   By: oexall <owen@exall.za.net>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/09 21:21:58 by oexall            #+#    #+#             */
-/*   Updated: 2016/08/09 21:30:36 by oexall           ###   ########.fr       */
+/*   Updated: 2016/08/10 13:28:26 by oexall           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,16 @@ void	ft_output_free(t_output **output)
 		{
 			tmp = list;
 			list = list->next;
-			free(tmp->opcode);
-			free(tmp->acb);
-			free(tmp->arg1);
-			free(tmp->arg2);
-			free(tmp->arg3);
-			free(tmp->arg4);
+			if (tmp->acb)
+				free(tmp->acb);
+			if (tmp->arg1)
+				free(tmp->arg1);
+			if (tmp->arg2)
+				free(tmp->arg2);
+			if (tmp->arg3)
+				free(tmp->arg3);
+			if (tmp->arg4)
+				free(tmp->arg4);
 			tmp->prev = NULL;
 			tmp->next = NULL;
 		}
