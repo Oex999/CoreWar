c623255464534e53c9c012aefed86677d050be8f
Gman513 <gregory.havenga@gmail.com>
2016-08-28 19:03:54 +0200

    Add instruction defined

--- vm/srcs/cmd1.c
+++ vm/srcs/cmd1.c
@@ -53,9 +53,24 @@ void		 	st(t_process *process)
 	}
 }
 
+/*3 registry coding bte = 84*/
+
 void			add(t_process *process)
 {
-	(void)process;
+    if (process->current_op.acb == 84)
+    {
+        if (process->current_op.arg1 < 17 && process->current_op.arg1 > 0)
+            if (process->current_op.arg2 < 17 && process->current_op.arg2 > 0)
+                if (process->current_op.arg3 < 17 && 
+                        process->current_op.arg3 > 0)    
+                {
+                    process->current_op.arg3 = 
+                        process->current_op.arg1 + process->current_op.arg2;
+                    process->carry = 1;
+                }
+    }
+    else
+        process->carry = 0;
 }
 
 void			sub(t_process *process)
